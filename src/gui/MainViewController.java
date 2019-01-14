8dcc044b2518b844e7e0869836c47fa057292e71
gustavoasebba <gustavoasebba@yahoo.com.br>
2019-01-14 12:43:57 -0200

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -12,7 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuItem; 
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 
@@ -34,7 +34,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 
 	@FXML
