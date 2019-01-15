288d195cadb1b5731134464d3086836045fb99bc
gustavoasebba <gustavoasebba@yahoo.com.br>
2019-01-15 12:08:23 -0200

    DepartmentForm (dialog) design

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -1,18 +1,17 @@
 package model.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 
-	public List<Department> findAll(){
-	 List<Department> List = new ArrayList<>();
-	 List.add(new Department(1, "Books"));
-	 List.add(new Department(2, "Computers"));
-	 List.add(new Department(3, "Electronics"));
-	 return List;
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+
+	public List<Department> findAll() {
+		return dao.findAll();
 	}
-	
+
 }
